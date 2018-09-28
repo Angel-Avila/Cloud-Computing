e89bb69af7fa1d5da808ba410c5d499f2a8664db
Angel Avila <aangel.aac96@gmail.com>
2018-09-28 12:35:54 -0500

    Examen 1 done

---
+++ examen1/lambda.js
@@ -0,0 +1,50 @@
+const AWS = require('aws-sdk');
+const rekognition = new AWS.Rekognition();
+
+exports.handler = async (event) => {
+    let fileName = event.fileName;
+    let data64 = event.data64;
+
+    try {
+        const body = await getLabels(data64);
+        const labels = body.Labels;
+    
+        var response = {
+            fileName: fileName,
+            Tags: labels
+        };
+    
+        return response;
+    } catch (err) {
+        var response = {
+            fileName: fileName,
+            Error: {
+                Error: err.message,
+                Description: err.stack
+            }
+        };   
+        return response;
+    }
+};
+
+const getLabels = async(data64) => {
+    var params = {
+        Image: { 
+            Bytes: new Buffer(data64, 'base64')
+        },
+        MaxLabels: 10,
+        MinConfidence: 75.0
+    };
+  
+    return rekognition.detectLabels(params, function(err, data) {
+        console.log("And here!");
+        if (err) {
+            //console.log(err, err.stack);
+            return err;
+        } 
+        else {
+            //console.log(data); 
+            return data;
+        }
+    }).promise();
+}
