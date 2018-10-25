a2c5fc40188419ac658d505293d0723bc450ef49
Angel Avila <aangel.aac96@gmail.com>
2018-10-25 17:54:52 -0500

    mapreduce working

--- practica2/src/mx/iteso/desi/cloud/hw2/GeocodeMapper.java
+++ practica2/src/mx/iteso/desi/cloud/hw2/GeocodeMapper.java
@@ -1,12 +1,55 @@
 package mx.iteso.desi.cloud.hw2;
 
-import mx.iteso.desi.cloud.GeocodeWritable;
+import mx.iteso.desi.cloud.*;
 
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.io.*;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class GeocodeMapper extends Mapper<LongWritable, Text, Text, GeocodeWritable> {
 
-  /* TODO: Your mapper code here */
-  
+    private final static GeocodeWritable one = new GeocodeWritable();
+    private Text word = new Text();
+
+    public static String textValue = "Point";
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        Triple tr;
+
+        StringTokenizer itr = new StringTokenizer(value.toString(),"\n");
+
+        while(itr.hasMoreTokens()) {
+
+            String token = itr.nextToken();
+
+            if(token.charAt(0) == '#' || token.equals("started"))
+                continue;
+
+            tr = ParseTriple.parseTriple(token);
+
+            if (tr.get(1).equals("http://www.georss.org/georss/point")) {
+                Double [] latLon = ParserCoordinates.parseCoordinates(tr.get(2));
+
+                Text text = new Text(textValue);
+                DoubleWritable lat = new DoubleWritable(latLon[0]);
+                DoubleWritable lon = new DoubleWritable(latLon[1]);
+
+                one.set(text, lat, lon);
+                word.set(tr.get(0));
+                context.write(word, one);
+            }
+            else if (tr.get(1).equals("http://xmlns.com/foaf/0.1/depiction")) {
+
+                Text text = new Text(tr.get(2));
+
+                one.set(text, new DoubleWritable(), new DoubleWritable());
+                word.set(tr.get(0));
+                context.write(word, one);
+            }
+
+        }
+    }
 }
--- practica2/src/mx/iteso/desi/cloud/hw2/GeocodeReducer.java
+++ practica2/src/mx/iteso/desi/cloud/hw2/GeocodeReducer.java
@@ -1,12 +1,60 @@
 package mx.iteso.desi.cloud.hw2;
 
+import mx.iteso.desi.cloud.Geocode;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.io.*;
 
 import mx.iteso.desi.cloud.GeocodeWritable;
 
+import java.io.IOException;
+
 public class GeocodeReducer extends Reducer<Text, GeocodeWritable, Text, Text> {
 
-  /* TODO: Your reducer code here */
+    private String place;
+    private double lat, lon;
+    private Geocode geocode;
+
+    private Geocode Guadalajara = new Geocode("Guadalajara", 20.66, -103.35);
+    private Geocode Monterrey = new Geocode("Monterrey", 25.69, -100.32);
+    private Geocode Philadelphia = new Geocode("Philadelphia", 39.95, -75.17);
+    private Geocode Houston = new Geocode("Houston", 29.77, -95.35);
+    private Geocode Seattle = new Geocode("Seattle", 47.61, -122.34);
+
+    public void reduce(Text key, Iterable<GeocodeWritable> values, Context context) throws IOException, InterruptedException {
+
+        for (GeocodeWritable g : values){
+            if(g.getName().toString().equals(GeocodeMapper.textValue)){
+                lat = g.getLatitude();
+                lon = g.getLongitude();
+            }
+            else {
+                place = g.getName().toString();
+            }
+        }
+
+        if (isInRange(lat, lon)) {
+            geocode = new Geocode(place, lat, lon);
+            Text text = new Text(geocode.toString());
+            context.write(key, text);
+        }
+    }
+
+    private Boolean isInRange(Double lat, Double lon) {
+        Geocode geocode = new Geocode("", lat, lon);
+
+        if (geocode.getHaversineDistance(Guadalajara.getLatitude(), Guadalajara.getLongitude()) <= 5000.00) {
+            return true;
+        }  else if (geocode.getHaversineDistance(Monterrey.getLatitude(), Monterrey.getLongitude()) <= 5000.00) {
+            return true;
+        }  else if (geocode.getHaversineDistance(Philadelphia.getLatitude(), Philadelphia.getLongitude()) <= 5000.00) {
+            return true;
+        }  else if (geocode.getHaversineDistance(Houston.getLatitude(), Houston.getLongitude()) <= 5000.00) {
+            return true;
+        }  else if (geocode.getHaversineDistance(Seattle.getLatitude(), Seattle.getLongitude()) <= 5000.00) {
+            return true;
+        }
+
+        return false;
+    }
   
 }
